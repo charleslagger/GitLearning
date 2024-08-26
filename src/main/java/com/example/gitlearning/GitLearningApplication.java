3e28f8f7287894bbc4c412010c9378b3ffcaddd8
charles <nvkhoeuet@gmail.com>
2024-08-26 23:11:41 +0700

    Initial commit

---
+++ src/main/java/com/example/gitlearning/GitLearningApplication.java
@@ -0,0 +1,14 @@
+package com.example.gitlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitLearningApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Commit rebase 2");
+        SpringApplication.run(GitLearningApplication.class, args);
+    }
+
+}
